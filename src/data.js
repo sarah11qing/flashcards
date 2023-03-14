12ae42474ec9ef4b3005aa4b1e54a1c8d9e74020
sarah11qing <sarah11qing@sina.com>
2023-03-13 23:26:32 -0700

    Add new data

--- src/data.js
+++ src/data.js
@@ -1,29 +1,27 @@
-export const appTitle = "The Ultimate Algorithms Master!";
-export const appSubtitle = "How good of do you master algorithms? Test all of your algorithms knowledge here!";
+export const appTitle = "The Ultimate Plant Master!";
+export const appSubtitle = "How good of do you master plants? Test all of your knowledge here!";
 
 export const cardSet = [
   {
     id: 0,
     front: {
-      text: "What is the difference between Memoization and Tabulation in Dynamic Programming?",
-      img: "",
+      text: "What is this type flowers?",
+      img: "https://upload.wikimedia.org/wikipedia/commons/a/ad/Lavender.JPG",
     },
     back: {
-      text: `
-      Memoization is a top-down approach where we cache the results of sub-problems during recursion, while tabulation is a bottom-up approach where we fill up a table or an array with the results of sub-problems in an iterative manner.
-      `,
-      img: "https://media.geeksforgeeks.org/wp-content/uploads/20220914114911/DynamicProgramming1.jpg",
+      text: "lavender",
+      img: "",
     },
     level: "easy",
   },
   {
     id: 1,
     front: {
-      text: "How to sort a dict in value's descending order?",
-      img: "",
+      text: "What type of trees has leaves like this?",
+      img: "https://www.biolandes.com/wp-content/uploads/photo-produit-MGL20054.jpg",
     },
     back: {
-      text: "Use <h1>sorted_list = sorted(map.items(), key = lambda x: -x[1])</h1>. If you want to sort in key's ascending order in the same time, use `sorted_list = sorted(map.items(), key=lambda x: (-x[1], x[0]))`",
+      text: "ginkgo",
       img: "",
     },
     level: "medium",
@@ -31,23 +29,23 @@ export const cardSet = [
   {
     id: 2,
     front: {
-      text: "What is Boyer-Moore majority vote algorithm?",
-      img: "",
+      text: "What is this type of flowers?",
+      img: "https://www.thespruce.com/thmb/wtmDeIpqa56LouWZJ65vUUg5ZBg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/growing-and-caring-for-hyacinth-plants-1402248-03-571d970f763b4c22ab2a77676160ee97.jpg",
     },
     back: {
-      text: `The Boyer-Moore majority vote algorithm is an algorithm for finding the majority of a sequence of elements using linear time and constant space.`,
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Boyer-Moore_MJRTY.svg/1920px-Boyer-Moore_MJRTY.svg.png",
+      text: `hyacinth`,
+      img: "",
     },
     level: "hard",
   },
   {
     id: 3,
     front: {
-      text: "What is Delayed Calculation?",
-      img: "",
+      text: "What is the Latin name of this type of plant?",
+      img: "https://media.karousell.com/media/photos/products/2021/6/9/peace_lily_spathiphyllum_lanci_1623244597_9aedc5d7_progressive.jpg",
     },
     back: {
-      text: "We can use delayed calculation to defer an operation until it is actually needed. An example is Design a Stack With Increment Operation.",
+      text: "Spathiphyllum lanceifolium",
       img: "",
     },
     level: "hard",
@@ -55,23 +53,23 @@ export const cardSet = [
   {
     id: 4,
     front: {
-      text: "What's the use of mono stack?",
-      img: "",
+      text: "What is this type of flowers?",
+      img: "https://upload.wikimedia.org/wikipedia/commons/0/09/Cosmos_bipinnatus_%282%29.jpg",
     },
     back: {
-      text: "A monotonic stack is a data structure that can be used to solve a variety of problems involving finding the next greater element, next smaller element, or similar problems that require identifying the nearest element with some specific property. A monotonic stack is a stack that either maintains a non-decreasing or a non-increasing order of its elements.",
-      img: "https://pbs.twimg.com/media/FCEzSnnUYAIt42h?format=png&name=900x900",
+      text: "Garden Cosmos",
+      img: "",
     },
     level: "medium",
   },
   {
     id: 5,
     front: {
-      text: "What is Greedy Algorithms?",
-      img: "",
+      text: "What is this type of plant? Tip: it can be used in cooking!",
+      img: "https://www.mashed.com/img/gallery/everything-you-need-to-know-about-rosemary/l-intro-1623689936.jpg",
     },
     back: {
-      text: "Greedy algorithms are a class of algorithms that make locally optimal choices at each step with the hope of finding a global optimum. In other words, at each step, the greedy algorithm chooses the option that seems best at that moment, without considering the future consequences or the overall optimal solution.",
+      text: "rosemary",
       img: "",
     },
     level: "easy",
